8f0a2172cb1d89d1740abf4da33ac1e3f07f0904
Georgette-Turner <63122409+Georgette-Turner@users.noreply.github.com>
2020-08-28 12:26:00 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Game from "./components/game/Game"
+import Antelopes from "./images/Antelope.jpg"
+import Cheetahs from "./images/Cheetahs.jpg"
+import Lemurs from "./images/Lemurs.jpg"
+import Ravens from "./images/Ravens.jpg"
+
 
 function App() {
+      
+  const cheetahs = {
+    name: 'Cheetahs', 
+    logoSrc: Cheetahs
+  }  
+  
+
+   const lemurs = {
+    name: 'Belleville Lemurs',
+    logoSrc: Lemurs
+  }
+
+  const antelope = {
+    name: 'Ann Arbor Antelopes',
+    logoSrc: Antelopes
+  }
+
+  const ravens = {
+    name: 'Romulus Ravens',
+    logoSrc: Ravens
+    } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game
+        venue="The Big House"
+        homeTeam={cheetahs}
+        visitingTeam={lemurs}
+      />
+      <Game
+        venue="Belleville Lake Arena"
+        homeTeam={antelope}
+        visitingTeam={ravens}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
